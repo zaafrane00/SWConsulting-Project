10713b428e5afa381cf8efa21d11ceea67379ac1
zaafrane00 <zaafranehamza@gmail.com>
2019-06-18 16:37:25 +0100

    tkmilet limigration nehar 2

--- database/migrations/2019_06_17_161128_create_categories_table.php
+++ database/migrations/2019_06_17_161128_create_categories_table.php
@@ -16,6 +16,7 @@ class CreateCategoriesTable extends Migration
         Schema::create('categories', function (Blueprint $table) {
             $table->bigIncrements('id_categories');
             $table->string('nom');
+            $table->string('icon');
             $table->timestamps();
         });
     }
---
+++ database/migrations/2019_06_18_135807_create_favories_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateFavoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('favories', function (Blueprint $table) {
+            $table->bigIncrements('id_favorie');
+            $table->bigInteger('id_user')->unsigned();
+            $table->foreign('id_user')->references('id')->on('users')->onDelete('cascade');
+            $table->bigInteger('id_prestataire')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('favories');
+    }
+}
